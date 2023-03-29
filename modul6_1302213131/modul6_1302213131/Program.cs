8d9dd6389b7622d421c7a98aa9eccd2207db9134
Marsyarayyy <marsyarh09@gmail.com>
2023-03-29 17:17:17 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_1302213131/modul6_1302213131/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.id = new Random().Next(5);
+            this.playcount = 0;
+        }
+        public void IncreasePlayCount(int playcount)
+        {
+            this.playcount += playcount;
+        }
+        public int GetTotalVideoPlaycount()
+        {
+            return playcount;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID        : " + id);
+            Console.WriteLine("Title     : " + title);
+            Console.WriteLine("Playcount : " + playcount);
+        }
+    }
+    public class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> UploadedVideos;
+        public string Username;
+        public SayaTubeUser(string Username,List<SayaTubeVideo> UploadedVideos)
+        {
+            this.Username = Username;
+        }
+        public void addVideo(SayaTubeVideo video)
+        {
+            this.UploadedVideos.Add(video);
+        }
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("User: " + Username);
+
+            Console.WriteLine();
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
